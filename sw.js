3ac0a432cf1b403fed1edcdf3dddb3d60eb8de0c
Julian Gonzalez <juliangon25@gmail.com>
2020-03-26 10:31:52 -0300

    manifest arreglado

--- sw.js
+++ sw.js
@@ -1,20 +1,21 @@
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v2'
+const STATIC_CACHE = 'static-v3'
 const DYNAMIC_CACHE = 'dynamic-v2'
 const INMUTABLE_CACHE = 'inmutable-v1'
 
 
 const APP_SHELL = [
     //'/',
-    '/index.html',
-    '/css/style.css',
-    '/img/favicon.ico',
-    '/img/avatars/hulk.jpg',
-    '/img/avatars/spiderman.jpg',
-    '/img/avatars/ironman.jpg',
-    '/img/avatars/thor.jpg',
-    '/img/avatars/wolverine.jpg',
+    'index.html',
+    'manifest.json',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
     'js/app.js',
     'js/sw-utils.js'
 ];
