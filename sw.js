5a2e8fa0c61828c266251b58fb4d70b2e1a4a1e4
Julian Gonzalez <juliangon25@gmail.com>
2020-03-26 09:53:50 -0300

    .

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v1'
-const DYNAMIC_CACHE = 'dynamic-v1'
+const STATIC_CACHE = 'static-v2'
+const DYNAMIC_CACHE = 'dynamic-v2'
 const INMUTABLE_CACHE = 'inmutable-v1'
 
 
@@ -49,6 +49,10 @@ self.addEventListener('activate', e => {
                 return caches.delete(key);
             }
 
+            if ( key !== DYNAMIC_CACHE && key.includes('dynamic')){
+                return caches.delete(key);
+            }
+
         });
 
     });
@@ -71,7 +75,7 @@ self.addEventListener('fetch', e => {
 
                 return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes );
 
-            }) 
+            });
 
         }
  
